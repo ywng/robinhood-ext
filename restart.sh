8c8b256da7acf2952466c23f1c33a214ee525480
Jason <victor.not.to.yield@gmail.com>
2019-04-20 22:46:25 -0400

    add Robinhood python unofficial APIs

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
-sudo pip install -r requirements.txt
-export FLASK_APP=server.py
 git pull
 
+sudo pip install -r requirements.txt
 sudo pip install ./Robinhood/
 
+export FLASK_APP=server.py
 flask run
 
